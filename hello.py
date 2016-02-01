75c215255a63e2f18d168b2bd36cad0397df3553
MariiaKravtsova <kravtsova.mariia@gmail.com>
2016-01-31 20:01:50 -0500

    Chapter 0 and 1

---
+++ hello.py
@@ -0,0 +1,11 @@
+print('What is your name, child?')
+childName = input()
+print('Are you lost ' + childName + '?')
+print('How old are you?')
+childAge = input()
+print(childAge + ' is my favorite age, you will be ' + str(int(childAge)+1) + ' in a year.')
+print()
+bacon = 20
+bacon = bacon + 1
+print(bacon)
+print('I have eaten ' + str(99) + ' burritos.')
