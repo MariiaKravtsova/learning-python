bcc05e0548bed0beadaa56fc7b9d0f42b8733bab
MariiaKravtsova <kravtsova.mariia@gmail.com>
2016-05-12 21:14:24 -0400

    more threads playing

--- smallExamples/threadDemo.py
+++ smallExamples/threadDemo.py
@@ -1,7 +1,9 @@
-import time, datetime
+import time, threading
 
-startTime = datetime.datetime(2029, 10, 31, 0, 0, 0)
-while datetime.datetime.now() < startTime:
-    time.sleep(1)
-
-print('Program now starting on Halloween 2029')
+print('Run')
+def pause():
+    time.sleep(3)
+    print('Resume')
+threadObj = threading.Thread(target=pause)
+threadObj.start()
+print('Stop')
