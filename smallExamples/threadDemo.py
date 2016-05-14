5f907354481909048e57b3c76b4bf630a2190b49
MariiaKravtsova <kravtsova.mariia@gmail.com>
2016-05-13 23:07:42 -0400

    sleep

--- smallExamples/threadDemo.py
+++ smallExamples/threadDemo.py
@@ -1,4 +1,10 @@
-import time, threading
+import time, threading, datetime
+
+startTime = datetime.datetime(2029, 10, 31, 0, 0, 0)
+while datetime.datetime.now() < startTime:
+    time.sleep(1)
+
+print('Program now starting on Halloween 2029')
 
 print('Run')
 def pause():
