84927921727f04f5b4f2262bf3d6565c9b5fdf75
MariiaKravtsova <kravtsova.mariia@gmail.com>
2016-07-02 11:22:39 -0400

    Generate population maps

--- pcc/project2/weather/high_lows.py
+++ pcc/project2/weather/high_lows.py
@@ -3,21 +3,23 @@ from matplotlib import pyplot as plt
 from datetime import datetime
 
 # Get dates, low and high temperatures from a csv file
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
     dates, lows, highs = [], [], []
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-
-        low = int(row[3])
-        lows.append(low)
-
-        high = int(row[1])
-        highs.append(high)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            low = int(row[3])
+            high = int(row[1])
+        except ValueError:
+            print(current_date, 'Missing data')
+        else:
+            dates.append(current_date)
+            lows.append(low)
+            highs.append(high)
 
 # Plot data
 fig = plt.figure(dpi=128, figsize=(10, 6))
@@ -26,7 +28,7 @@ plt.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format Plot
-plt.title('Daily high and low temperatures 2014', fontsize=20)
+plt.title('Daily high and low temperatures 2014\nDeath Valle, CA', fontsize=20)
 plt.xlabel('', fontsize=14)
 fig.autofmt_xdate()
 plt.ylabel('Temperatures', fontsize=14)
