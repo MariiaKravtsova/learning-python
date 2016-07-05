9cd7ac3142be9b6caaed0d7804d593dab6c017d7
MariiaKravtsova <kravtsova.mariia@gmail.com>
2016-07-05 17:51:55 -0400

    Oh, that is what lists are about

---
+++ pcc/lists/names.py
@@ -0,0 +1,5 @@
+names = ['Harry Potter', 'Hermoine Granger', 'Ron Weasly']
+names.remove('Ron Weasly')
+names.append('Hagrid')
+for name in names:
+    print('Hello, ' + name + ' would you join me for dinner?')
